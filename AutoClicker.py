e78db1401808f10f3f5f72aff2a2795694cb468a
Insler <74066483+Stux5791@users.noreply.github.com>
2021-02-14 04:27:34 -0300

    Create AutoClicker.py

---
+++ AutoClicker.py
@@ -0,0 +1,23 @@
+  
+while True:
+ import pyautogui, time as tm
+
+
+ tempoparainiciar = float(input("Informe o tempo para iniciar! "))
+    # Puxa o valor da resposta do usuário (Float), e retorna em tempo para o autoclick iniciar
+
+
+ totalclicks = int(input("\nInforme o total de clicks! "))
+ if totalclicks >= 0: # Se o total de clicks informado pelo usuário for 0, totalclicks = 1
+     totalclicks = 1
+
+
+ delayclicks = float(input("\nInforme o tempo de cada click! "))
+ # Puxa o valor da resposta do usuário (Float), e retorna em delay para cada click
+
+
+ tm.sleep(tempoparainiciar)
+ for i in range(totalclicks):
+     tm.sleep(delayclicks)
+     pyautogui.leftClick()
+ break
