b6775028683c7ddb6e84e2de753516981547ff24
Insler <74066483+Stux5791@users.noreply.github.com>
2021-02-15 16:27:10 -0300

    Update AutoClick.py

--- AutoClick.py
+++ AutoClick.py
@@ -1,18 +1,19 @@
+from colorama import Fore
 import pyautogui
 import time
 
 while True:
 
-    tempoparainiciar = float(input("\nInforme o tempo para iniciar em segundos! "))
+    tempoparainiciar = float(input(Fore.GREEN + "\nInforme o tempo para iniciar em segundos! "))
         # Puxa o valor da resposta do usuário (Float), e retorna em tempo para o autoclick iniciar
 
 
-    totalclicks = int(input("\nInforme o total de clicks! "))
+    totalclicks = int(input(Fore.GREEN + "\nInforme o total de clicks! "))
     if totalclicks <= 0: # Se o total de clicks informado pelo usuário for menor ou igual a 0, totalclicks = 1
         totalclicks = 1
 
 
-    delayclicks = float(input("\nInforme o tempo de cada click em segundos! "))
+    delayclicks = float(input(Fore.GREEN + "\nInforme o tempo de cada click em segundos! "))
         # Puxa o valor da resposta do usuário (Float), e retorna em delay para cada click
 
 
